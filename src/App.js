4e751acda385088f02233fca0aa076094e081323
Esha Vigneswaran <eshavigneswaran@gmail.com>
2025-03-29 16:15:50 -0400

    Updated App UI

--- src/App.js
+++ src/App.js
@@ -17,35 +17,41 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+      <h1 className = "text-6xl font-bold mb-[10px]">Welcome to Next Stop!</h1>
+      <p className = "text-xl mb-[30px]">Your go-to companion for entertainment during train delays.</p>
+
       <form
-        className="w-[860px] h-[550px] flex flex-col gap-4"
+        className="w-[460px] flex flex-col justify-center gap-4"
         onSubmit={handleSubmit}
       >
-        <label className="text-black">Enter your train number:</label>
         <input
           type="text"
-          className="bg-gray-700 p-2 rounded-md"
+          className="bg-gray-700 p-2 rounded-md text-white"
           value={trainNumber}
+          placeholder='Enter your train number'
+
           onChange={(e) => setTrainNumber(e.target.value)}
         />
-        <label className="text-black">Enter your location:</label>
         <input
           type="text"
-          className="bg-gray-700 p-2 rounded-md"
+          className="bg-gray-700 p-2 rounded-md text-white"
+          placeholder='Enter your location'
           value={location}
           onChange={(e) => setLocation(e.target.value)}
         />
-        <button type="submit" className="bg-blue-500 p-2 rounded-md text-white">
+
+        <button type="submit" className="bg-blue-500 p-2 rounded-md text-white w-[100px]">
           Submit
         </button>
       </form>
-      {trainData && (
+      
+      {/*trainData && (
         <div>
           <h2>Train Data:</h2>
           <pre>{JSON.stringify(trainData, null, 2)}</pre>
         </div>
-      )}
+      )*/}
     </div>
   );
 }
